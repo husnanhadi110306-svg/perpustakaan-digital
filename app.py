5acd604bf5da7d44542f665409045486b2946eb6
Husnan Hadi <husnanhadi110306@gmail.com>
2026-05-25 10:16:22 +0800

    final deploy

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
+
+# PENTING
+app = app
